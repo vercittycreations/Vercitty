6949094e41464209f9ee29e54cd58c8971075cca
vercittycreations <vercittycreations@gmail.com>
2026-01-30 19:03:33 +0530

    .

--- src/app/components/PortfolioSection.tsx
+++ src/app/components/PortfolioSection.tsx
@@ -33,6 +33,15 @@ const projects = [
     color: 'from-cyan-400 to-blue-400',
   },
   {
+    id: 'quit-desktop',
+    title: 'Personal Dairy Webapp',
+    category: 'Web Development',
+    url: 'https://quit-desktop.vercel.app/',
+    description: 'Personal Dairy Task app with multi user login with firebase auth.',
+    image: 'https://images.unsplash.com/photo-1579017308347-e53e0d2fc5e9?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    color: 'from-cyan-400 to-blue-400',
+  },
+  {
     id: 'SaaS',
     title: 'SasS Platform Web',
     category: 'Web Development',
